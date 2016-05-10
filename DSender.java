9ff7138217d317475ad37f2b39e67d19b385b0a5
Brandon McCoy <hendrix7@student.clackamas.edu>
2016-05-09 19:04:09 -0700

    Update DSender.java

--- DSender.java
+++ DSender.java
@@ -3,40 +3,39 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 /**
- * <h1>DSender</h1>
  * The DSender class creates a DatagramPacket containing a String message,
  * that is then sent to an IP address (String representation of the IP address)
  * through a specific port. These three arguments are parameters 
  * of the DSender class constructor.
- * 
- * @author Brandon McCoy
- * @date 05/09/2016
- * 
- */
-
-/* The range of port numbers from 1024 to 49151 are the registered ports. 
+ * <p>
+ * The range of port numbers from 1024 to 49151 are the registered ports. 
  * They are assigned by IANA for specific service upon application by 
  * a requesting entity.
  * 
  * 666  is the official Doom fps socket
  * 2302 is the unofficial Halo: Combat Evolved multiplayer socket
  * 3071 is the unofficial Call of Duty Black Ops socket
- * 3659	is the unofficial Battlefield 4 socket
- * 
+ * 3659 is the unofficial Battlefield 4 socket
+ * <p>
  * https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
- * */
+ * 
+ * @author Brandon McCoy
+ * @since 05/09/2016
+ */
 
 public class DSender {
 	
 	/**
-	 * <b>DSender constructor</b> 
+	 * <b>DSender constructor</b> creates a DatagramPacket containing a String message,
+	 * that is then sent to an IP address (String representation of the IP address)
+	 * through a specific port.
 	 * <p>
 	 * <b>Note:</b> DatagramPacket delivery is not guaranteed.
 	 * 
-	 * @param String mMessage - The message to be sent to the server
-	 * @param String mInetAddressName - The name of the host
-	 * @param int mPort - The port the DatagramPacket is sent on
-	 * @throws Exception - 
+	 * @param mMessage The message to be sent to the server
+	 * @param mInetAddressName The name of the host
+	 * @param mPort The port the DatagramPacket is sent on
+	 * @throws Exception  
 	 */
 	public DSender(String mMessage, String mInetAddressName, int mPort) 
 			throws Exception {
@@ -52,7 +51,7 @@ public class DSender {
 		
 		ds.close();
 	}
-	
+
 	// TODO main function will be omitted
 	public static void main(String[] args) throws Exception {
 		InetAddress ip = InetAddress.getLocalHost();
