0d8308ec999d9d125c020c697cfb238224edebb0
Brandon McCoy <hendrix7@student.clackamas.edu>
2016-05-09 19:05:19 -0700

    Update DReceiver.java

--- DReceiver.java
+++ DReceiver.java
@@ -8,11 +8,15 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.InetAddress;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
+/**
+ * 
+ * @author Brandon McCoy
+ * @Since 05/09/2016
+ *
+ */
 public class DReceiver {
 	
 	JFrame frame = new JFrame("My Window");
@@ -20,10 +24,13 @@ public class DReceiver {
 	JLabel lblMyPort = new JLabel();
 	JLabel lblMyIP = new JLabel();
 	Insets lblInsets = new Insets(10, 50, 10, 10);
-	
+
+	/**
+	 * 
+	 */
 	public DReceiver(){
 		frame.setLayout(new GridBagLayout());
-		
+
 		addComponent(frame, lblMyIP, 0, 0,
 				1, 1, GridBagConstraints.WEST, 
 				GridBagConstraints.NONE, lblInsets);
@@ -33,7 +40,7 @@ public class DReceiver {
 		addComponent(frame, lblMessage, 0, 2,
 				1, 1, GridBagConstraints.WEST, 
 				GridBagConstraints.NONE, lblInsets);
-		
+
 		try {
 			this.loadHostAddress();
 		} catch (Exception e) {
@@ -48,35 +55,54 @@ public class DReceiver {
 		frame.setVisible(true);
 	}
 	
-	// addComponent
-		// An easier way of creating GridBagRestraints, and then adding a component to the container
-		private static void addComponent(Container container, Component component, int gridx, int gridy,
-				int gridwidth, int gridheight, int anchor, int fill, Insets insets) {
-			GridBagConstraints gbc = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 1.0, 1.0,
-					anchor, fill, insets, 0, 0);
-			container.add(component, gbc);
-		}
-		
-		private void loadHostAddress() throws Exception{
-	    	InetAddress inet = Inet4Address.getLocalHost();
-	    	lblMyIP.setText("My Local IP Address: " + inet.getHostAddress());
-	    }
-	
+	/**
+	 * An easier way of creating GridBagRestraints, and then adding a component to the container
+	 * 
+	 * @param container
+	 * @param component
+	 * @param gridx
+	 * @param gridy
+	 * @param gridwidth
+	 * @param gridheight
+	 * @param anchor
+	 * @param fill
+	 * @param insets
+	 */
+	private static void addComponent(Container container, Component component, int gridx, int gridy,
+			int gridwidth, int gridheight, int anchor, int fill, Insets insets) {
+		GridBagConstraints gbc = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 1.0, 1.0,
+				anchor, fill, insets, 0, 0);
+		container.add(component, gbc);
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	private void loadHostAddress() throws Exception{
+		InetAddress inet = Inet4Address.getLocalHost();
+		lblMyIP.setText("My Local IP Address: " + inet.getHostAddress());
+	}
+
+	/**
+	 * 
+	 * @throws IOException
+	 */
 	public void listen() throws IOException{
 		DatagramSocket ds = new DatagramSocket(3659);
 		Integer portNumber = ds.getLocalPort();
 		lblMyPort.setText("My Local Port: " + portNumber.toString());
-		
+
 		byte[] buf = new byte[1024];
 		DatagramPacket dp = new DatagramPacket(buf, 1024);
 		ds.receive(dp);
 		String str = new String(dp.getData(), 0, dp.getLength());
-		
+
 		lblMessage.setText("Incoming Message: " + str);
-		
+
 		ds.close();
 	}
-	
+
 	public static void main(String[] args) {
 		DReceiver rec = new DReceiver();
 		while(true){
